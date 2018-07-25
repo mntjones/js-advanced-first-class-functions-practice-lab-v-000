f2592e5842abde47f9268900db05bc8badd7b13e
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-25 19:16:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,7 @@ function logDriversByHometown(drivers, location) {
 }
 
 function driversByRevenue(drivers) {
-  return drivers.sort();
+  return drivers.sort(function (a, b) {
+  return a.revenue - b.revenue;
+});
 }
