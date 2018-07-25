abbaed4fed67d6b166ba1001a06aaed8e8269bae
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-25 18:59:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
 function logDriverName(drivers) {
-  drivers.forEach
+  drivers.forEach(function(element) { console.log(element.name)});
+}
+
+function logDriverByHometown(drivers, location) {
+  
 }
