121f9b5ef11da7905f92b490d8ba3171a151c7cb
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-25 19:12:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function logDriverNames(drivers) {
   drivers.forEach(function(element) { console.log(element.name)});
 }
 
-function logDriverByHometown(drivers, location) {
+function logDriversByHometown(drivers, location) {
   const filteredDrivers = drivers.filter (function(driver) {return driver.hometown === location });
   logDriverName(filteredDrivers);
 }
